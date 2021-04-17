305827cacf1b9b8ec05c71c99a4a8ecbf5f23c7a
Chouaib-Mrabet <Mrabet.Chouaib.98@gmail.com>
2021-04-17 10:21:40 +0100

    first commit : Court Reservation schemas

---
+++ models/Client.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ClientSchema = mongoose.Schema({
+    _id: Schema.Types.ObjectId,
+    name: {
+        type: String,
+        require: true
+    },
+    cin: {
+        type : Number,
+        require: true,
+        unique: true
+    },
+    phone: {
+        type: Number,
+        require: true,
+        unique : true
+    },
+    numberOfPreviousReservation: {
+        type : Number,
+        require: true,
+        default : 0
+    },
+    rating :{ // Rating by the court owner :
+        type: Number,
+        require:true,
+        default : -1 // This client has no rating yet
+    }
+});
+
+module.exports = mongoose.model('Court', CourtSchema);
